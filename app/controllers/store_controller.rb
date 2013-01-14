cf42f99322efa689dcf25515a168527e5b84f160
Dusan Orlovic <orlovics@eunet.rs>
2013-01-14 14:18:02 +0100

    adding realtionships, Add to Cart button some test

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    if session[:counter].nil?
+      @counter=1
+    else
+      @counter = session[:counter] + 1
+    end
+    session[:counter] = @counter
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,3 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items, :dependent => :destroy #brisanjem carta, pronaci i obrisati sve line_items sa odgovarajucim cart_id
 end
