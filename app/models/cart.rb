bc9b5bbec26ceacf59d7fc7906ebba8b92e29483
Dusan Orlovic <orlovics@eunet.rs>
2013-01-15 08:51:28 +0100

    logger.error for invalid cart.id, delete cart button, sum of line_items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,10 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy #brisanjem carta, pronaci i obrisati sve line_items sa odgovarajucim cart_id
 
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
     if current_item
