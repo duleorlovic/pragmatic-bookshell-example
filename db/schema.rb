1c0f28af063a2d5ed4e32efa122de7f52ac91104
Dusan Orlovic <orlovics@eunet.rs>
2013-01-23 10:52:04 +0100

    rails generate scaffold user name:string hashed_password:string salt:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122210242) do
+ActiveRecord::Schema.define(:version => 20130123095109) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(:version => 20130122210242) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
